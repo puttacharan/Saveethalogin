d2017f35507d21ca96b40c732cb17046cffed796
puttacharan <puttasusmi444@gmail.com>
2020-09-11 13:57:08 +0530

    commit msg 2

---
+++ app/src/main/java/com/example/saveethalogin/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.saveethalogin;
+
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    public WebView webview;
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading ...");
+        progressDialog.setCancelable(false);
+        webview = findViewById(R.id.webview);
+        webview.requestFocus();
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setSavePassword(true);
+        webview.loadUrl("http://arms.sse.saveetha.com/");
+        webview.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webview.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                if (progress < 100) {
+                    progressDialog.show();
+                }
+                if (progress == 100) {
+                    progressDialog.dismiss();
+                }
+            }
+        });
+    }
+}
